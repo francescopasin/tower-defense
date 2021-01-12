77c0ebb924a457ab3ed4c467e26cb66ac6a61617
Urbani Simone <urb.simone@gmail.com>
2021-01-12 10:05:09 +0100

    Fix Wave

--- model/wave.cpp
+++
@@ -1,6 +0,0 @@
-#include "wave.h"
-
-namespace model {
-Wave::Wave(float health, U_INT speed, U_INT enemyNumber, U_INT enemyIntervalTick, U_INT startAfter) : _health(health), _speed(speed), _enemyNumber(enemyNumber), _enemyIntervalTick(enemyIntervalTick), _startAfter(startAfter) {}
-
-}  // namespace model
