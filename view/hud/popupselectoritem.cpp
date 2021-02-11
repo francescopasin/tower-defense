870929cb606a75fe5df82908e0c7aab33d6300ed
francescopasin <francypasi42@gmail.com>
2021-02-11 10:31:10 +0100

    Refactored popup selector item

---
+++ view/hud/popupselectoritem.cpp
@@ -0,0 +1,53 @@
+#include "view/hud/popupselectoritem.h"
+
+#include <QCursor>
+#include <QPainter>
+#include <QStyleOptionGraphicsItem>
+
+namespace view {
+
+PopupSelectorItem::PopupSelectorItem(QGraphicsItem *parent, const QString &imagePath)
+    : QGraphicsItem(parent), _imagePath(imagePath), isAvailable(true) {
+    updateCursor();
+}
+
+QRectF PopupSelectorItem::boundingRect() const {
+    return QRectF(0, 0, 80, 80);
+}
+
+void PopupSelectorItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
+
+    QPixmap pixmap = QPixmap(_imagePath);
+
+    painter->setPen(Qt::NoPen);
+    painter->drawPixmap(QRect(0, 0, 80, 80), pixmap);
+}
+
+void PopupSelectorItem::updateCursor() {
+    if (isAvailable) {
+        QPixmap pixmap = QPixmap(":/assets/images/pointers/pointer-interactive.png");
+        setCursor(QCursor(pixmap.scaled(32, 32)));
+        setOpacity(1);
+    } else {
+        QPixmap pixmap = QPixmap(":/assets/images/pointers/pointer-not-allowed.png");
+        setCursor(QCursor(pixmap.scaled(32, 32)));
+        setOpacity(0.5);
+    }
+}
+
+void PopupSelectorItem::mousePressEvent(QGraphicsSceneMouseEvent *event) {
+    Q_UNUSED(event);
+
+    if (isAvailable) {
+        emit pressed();
+    }
+}
+
+void PopupSelectorItem::setAvailability(bool availability) {
+    isAvailable = availability;
+    updateCursor();
+}
+
+}  // namespace view
--- view/screens/setMapScreen/cellselector.cpp
+++ view/screens/setMapScreen/cellselector.cpp
@@ -3,8 +3,6 @@
 #include <QCursor>
 #include <QPainter>
 
-#include "view/screens/setMapScreen/cellselectoritem.h"
-
 namespace view {
 
 CellSelector::CellSelector() {
@@ -13,49 +11,17 @@ CellSelector::CellSelector() {
     QPixmap pixmap = QPixmap(":/assets/images/pointers/pointer.png");
     setCursor(QCursor(pixmap.scaled(32, 32)));
 
-    CellSelectorItem *freeCell = new CellSelectorItem(this, SetMapCell::Type::Free);
+    freeCell = new PopupSelectorItem(this, ":/assets/images/tiles/general-path-tile.png");
     freeCell->setPos(10, 10);
-    connect(freeCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::Free); });
+    connect(freeCell, &PopupSelectorItem::pressed, this, [=]() { emit cellSelected(GridCellType::Path); });
 
-    CellSelectorItem *blockedCell = new CellSelectorItem(this, SetMapCell::Type::Blocked);
+    PopupSelectorItem *blockedCell = new PopupSelectorItem(this, ":/assets/images/tiles/blocked-tile.png");
     blockedCell->setPos(110, 10);
-    connect(blockedCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::Blocked); });
-
-    CellSelectorItem *startCell = new CellSelectorItem(this, SetMapCell::Type::Start);
-    startCell->setPos(210, 10);
-    connect(startCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::Start); });
-
-    CellSelectorItem *finishCell = new CellSelectorItem(this, SetMapCell::Type::Finish);
-    finishCell->setPos(310, 10);
-    connect(finishCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::Finish); });
-
-    CellSelectorItem *orizzCell = new CellSelectorItem(this, SetMapCell::Type::Orizz);
-    orizzCell->setPos(410, 10);
-    connect(orizzCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::Orizz); });
-
-    CellSelectorItem *vertCell = new CellSelectorItem(this, SetMapCell::Type::Vert);
-    vertCell->setPos(10, 110);
-    connect(vertCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::Vert); });
-
-    CellSelectorItem *sxupCell = new CellSelectorItem(this, SetMapCell::Type::SxUp);
-    sxupCell->setPos(110, 110);
-    connect(sxupCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::SxUp); });
-
-    CellSelectorItem *dxupCell = new CellSelectorItem(this, SetMapCell::Type::DxUp);
-    dxupCell->setPos(210, 110);
-    connect(dxupCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::DxUp); });
-
-    CellSelectorItem *sxdwCell = new CellSelectorItem(this, SetMapCell::Type::SxDw);
-    sxdwCell->setPos(310, 110);
-    connect(sxdwCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::SxDw); });
-
-    CellSelectorItem *dxdwCell = new CellSelectorItem(this, SetMapCell::Type::DxDw);
-    dxdwCell->setPos(410, 110);
-    connect(dxdwCell, &CellSelectorItem::pressed, this, [=]() { emit cellSelected(SetMapCell::Type::DxDw); });
+    connect(blockedCell, &PopupSelectorItem::pressed, this, [=]() { emit cellSelected(GridCellType::Blocked); });
 }
 
 QRectF CellSelector::boundingRect() const {
-    return QRectF(0, 0, 500, 200);
+    return QRectF(0, 0, 200, 200);
 }
 
 void CellSelector::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
@@ -68,7 +34,11 @@ void CellSelector::paint(QPainter *painter, const QStyleOptionGraphicsItem *opti
 
     painter->setPen(pen);
     painter->setBrush(QBrush(QColor::fromRgb(70, 70, 70)));
-    painter->drawRect(0, 0, 500, 200);
+    painter->drawRect(0, 0, 200, 200);
+}
+
+void CellSelector::setFreeCellAvailability(bool availability) {
+    freeCell->setAvailability(availability);
 }
 
 void CellSelector::mousePressEvent(QGraphicsSceneMouseEvent *event) {
